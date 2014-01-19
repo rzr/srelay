26045340bf232a33b290853de974c0f13105b1b9
Philippe Coval <rzr@gna.org>
2014-01-19 19:54:15 +0100

    dh
    Signed-off-by: Philippe Coval <rzr@gna.org>

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(srelay):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="srelay" command="/usr/bin/srelay"
---
+++ debian/srelay.default.ex
@@ -0,0 +1,10 @@
+# Defaults for srelay initscript
+# sourced by /etc/init.d/srelay
+# installed at /etc/default/srelay by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
